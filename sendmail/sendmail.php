95e9afe665270950f429d75f493a0217611a2599
belyjnikolaj <117487574+belyjnikolaj@users.noreply.github.com>
2023-11-07 19:17:25 +0200

    Add progect

---
+++ sendmail/sendmail.php
@@ -0,0 +1,61 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/SMTP.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('en', 'phpmailer/language/');
+    $mail->IsHTML(true);
+
+    $mail->isSMTP();//Send using SMTP
+    $mail->Host = 'smtp.gmail.com';//Send the SMTP server to send throuth
+    $mail->SMTPAuth = true;//Enable SMTP authentification
+    $mail->Username = 'napriklad950@gmail.com';//SMTP username (email)
+    $mail->Password = '';//SMTP password (email password)
+    $mail->Port = '587';
+    $mail->SMTPSecure = 'TLS';
+
+    //From SMTP username (email)
+    $mail->setFrom('napriklad950@gmail.com', 'Code Test');
+    //To...
+    $mail->addAddress('naprikladse9@gmail.com');
+    //Subject
+    $mail->Subject = 'E-mail from test';
+    //Body
+    $body = '<h1>Hi! It`s Test!</h1>';
+
+    if(trim(!empty($_POST['name']))) {
+        $body .= "<p>Name: <strong>".$_POST['name']."</strong></p>" 
+    }
+    if(trim(!empty($_POST['email']))) {
+        $body .= "<p>E-mail: <strong>".$_POST['email']."</strong></p>" 
+    }
+    if(trim(!empty($_POST['message']))) {
+        $body .= "<p>Message: <strong>".$_POST['message']."</strong></p>" 
+    }
+    if(trim(!empty($_POST['like']))) {
+        $body .= "<p>Do you like Test? <strong>".$_POST['like']."</strong></p>" 
+    }
+    if(trim(!empty($_POST['thebest']))) {
+        $body .= "<strong><a href='https://www.youtube.com/@codeonly'>Test</a> the best channel in the world</strong>" 
+    }
+
+    //Add file
+    if(trim(!empty($_FILES['image']['tmp_name']))) {
+        $_fileTmpName = $_FILES['image']['tmp_name'];
+        $_fileName = $_FILES['image']['name'];
+        $mail->addAttachment($_fileTmpName, $_fileName);
+    }
+
+    $mail->Body = $body;
+
+    //Sending
+    $mail->send();
+    $mail->smtpClose();
+?>
+
