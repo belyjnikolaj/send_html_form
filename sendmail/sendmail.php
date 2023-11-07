ed00c00ed1f3865263cfe07e8bbc425643500e6c
belyjnikolaj <117487574+belyjnikolaj@users.noreply.github.com>
2023-11-07 20:09:21 +0200

    add fix

--- sendmail/sendmail.php
+++ sendmail/sendmail.php
@@ -15,33 +15,33 @@
     $mail->isSMTP();//Send using SMTP
     $mail->Host = 'smtp.gmail.com';//Send the SMTP server to send throuth
     $mail->SMTPAuth = true;//Enable SMTP authentification
-    $mail->Username = 'napriklad950@gmail.com';//SMTP username (email)
-    $mail->Password = '';//SMTP password (email password)
+    $mail->Username = 'naprikladse9@gmail.com';//SMTP username (email)
+    $mail->Password = 'Neskashu@nap';//SMTP password (email password)
     $mail->Port = '587';
     $mail->SMTPSecure = 'TLS';
 
     //From SMTP username (email)
-    $mail->setFrom('napriklad950@gmail.com', 'Code Test');
+    $mail->setFrom('naprikladse9@gmail.com', 'Code Test');
     //To...
-    $mail->addAddress('naprikladse9@gmail.com');
+    $mail->addAddress('w27ksyu@gmail.com');
     //Subject
     $mail->Subject = 'E-mail from test';
     //Body
     $body = '<h1>Hi! It`s Test!</h1>';
 
-    if(trim(!empty($_POST['name']))) {
+    if(!empty($_POST['name'])) {
         $body .= "<p>Name: <strong>".$_POST['name']."</strong></p>" 
     }
-    if(trim(!empty($_POST['email']))) {
+    if(!empty($_POST['email'])) {
         $body .= "<p>E-mail: <strong>".$_POST['email']."</strong></p>" 
     }
-    if(trim(!empty($_POST['message']))) {
+    if(!empty($_POST['message'])) {
         $body .= "<p>Message: <strong>".$_POST['message']."</strong></p>" 
     }
-    if(trim(!empty($_POST['like']))) {
+    if(!empty($_POST['like'])) {
         $body .= "<p>Do you like Test? <strong>".$_POST['like']."</strong></p>" 
     }
-    if(trim(!empty($_POST['thebest']))) {
+    if(!empty($_POST['thebest'])) {
         $body .= "<strong><a href='https://www.youtube.com/@codeonly'>Test</a> the best channel in the world</strong>" 
     }
 
